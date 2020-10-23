29bb5018edb0af691fd569002747fe147e88668a
Apogee-Ind <jscales@apgind.com>
2020-10-22 20:13:21 -0500

    deleted a fuckton of useless build files

--- .build/obj_apgmkv36_windows/common/util.d
+++
@@ -1,9 +0,0 @@
-.build/obj_apgmkv36_windows/common/util.o: tmk_core/common/util.c \
- keyboards/apgmkv36/config.h quantum/config_common.h \
- tmk_core/common/util.h
-
-keyboards/apgmkv36/config.h:
-
-quantum/config_common.h:
-
-tmk_core/common/util.h:
--- .build/obj_mathkeeb_linux_variant/common/util.d
+++
@@ -1,9 +0,0 @@
-.build/obj_mathkeeb_linux_variant/common/util.o: tmk_core/common/util.c \
- keyboards/mathkeeb/config.h quantum/config_common.h \
- tmk_core/common/util.h
-
-keyboards/mathkeeb/config.h:
-
-quantum/config_common.h:
-
-tmk_core/common/util.h:
